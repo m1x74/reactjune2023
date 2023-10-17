a83f1416b008b8bb178802f80ebd2dd890874b14
m1x74 <stasik747474@gmail.com>
2023-10-17 21:37:22 +0300

    les1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import {Simpsons} from "./components/Simpsons/Simpsons";
+import {RickandMorty} from "./components/RickandMorty/RickandMorty";
+
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Simpsons/>
+      </div>
+        <hr/>
+        <div>
+            <RickandMorty/>
+        </div>
+
+
+    </div>
+  );
+}
+
+export default App;
